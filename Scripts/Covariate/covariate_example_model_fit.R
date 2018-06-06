5d6fc02f73ace35151149df8f6e0224f4eebbcf3
Brent Pease <bspease@ncsu.edu>
2018-06-06 15:42:52 -0400

    scripts for covariate model

---
+++ Scripts/Covariate/covariate_example_model_fit.R
@@ -0,0 +1,65 @@
+## Dependencies ##
+library(R2WinBUGS)
+library(here)
+
+BD <-  "path-to-winbugs-on-your-machine"
+
+#Load point-level (primary) data, neighborhood information, and grid (secondary) data
+source('Scripts/Covariate/data-prep.R') # (ART 1 minute)
+
+### ### ### ### ### ### ### ### ### ### ### ###
+
+# MCMC Settings
+ni = 50000
+nb = 10000
+nt = 50
+nc = 3
+
+### ### ### ### ### ### ### ### ### ### ### ###
+
+# Prepare data, set initial values, and indicate which values to be returned from BUGS #
+
+
+#Bundle data
+
+car.data <- list(effort = EE,
+                 count = YY,
+                 Y = grid1$total.dets,
+                 num = grid1.wbnb$num, 
+                 adj = grid1.wbnb$adj,
+                 weights = grid1.wbnb$weights,
+                 ncell = length(grid1.wbnb$num),
+                 cell = grid1$nearest_block_BBA_ID,
+                 nsite = nrow(grid1),
+                 forest = block$Forest1)
+
+
+#Set initial values
+car.inits <- function() {
+  list(z = as.numeric(grid1$total.dets>0),
+       b.forest = runif(1, -1, 1),
+       b.effort=runif(1,-1,1),
+       b.count=runif(1,-1,1),
+       p = runif(1),
+       alpha = runif(1,-1,1),
+       sd=runif(1,0.1,2),
+       S = c(rep(0,length(grid1.wbnb$num))))
+}
+
+
+#parameters to monitor
+
+car.parameters <- c('psi', 'p', "alpha", 'b.forest','b.effort','b.count')
+
+
+out <- bugs(data = car.data,
+            inits = car.inits,
+            parameters.to.save = car.parameters,
+            model.file = here("Models/covariate.txt"),
+            n.chains = nc,
+            n.thin = nt,
+            n.iter = ni,
+            n.burnin = nb,
+            bugs.dir = BD,
+            working.directory = here('Output'),
+            debug = F)
